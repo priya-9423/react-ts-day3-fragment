f99aa2cd998eaf1e3545b4e03fe25a67977f6c3d
priya-9423 <gatpriyanka1293@gmail.com>
2019-11-28 10:56:09 +0530

    1 file modified, 1 added

---
+++ SpreadOperator.tsx
@@ -0,0 +1,4 @@
+// import React, {Component} from 'react';
+// import ReactDOM from 'react-DOM';
+
+// export default
--- index.tsx
+++ index.tsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
 
-interface AppProps { }
+interface AppProps {}
 interface AppState {
   name: string;
 }
@@ -12,20 +12,42 @@ class App extends Component<AppProps, AppState> {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'React'
+      name: "React",
+      lName: "test",
+      salary: 1000
     };
+    var test1 = {
+      name: "React",
+      lName: "test",
+      salary: 1000
+    };
+    var test2 = { ...test1, salary: 111 };
+    console.log(test1);
+    console.log(test2);
+
+    var arr = ["test1", "2", "3"];
+    var arr2 = [...arr, "4"];
+
+    console.log(arr);
+    console.log(arr2);
+
+    var test3 = Object.assign({},test1, {salary:111});
+    console.log(test3);
+
+//object distructuring
+    let {lName: t1, ...all} = test1;
+    console.log(t1);
+    console.log(all);
   }
 
   render() {
     return (
       <div>
         <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        <table />
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
